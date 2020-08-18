9dd93699f35c0a4336375fb7792a2319874d948e
shinjh0849 <shinjh0849@gmail.com>
2020-08-18 22:34:57 +0900

    Add missing header, label

--- gv_ae_test.py
+++ gv_ae_test.py
@@ -153,7 +153,8 @@ def loadGumVec(train_file, train_label, test_file, test_label):
                                              'sha_BBFC'
                                              'sha_BFC',
                                              'key',
-                                             'project'])
+                                             'project',
+                                             'label'])
     testX = np.asarray(list(testX))
     testY = pd.read_csv(test_label, names=['index',
                                            'path_BBIC',
