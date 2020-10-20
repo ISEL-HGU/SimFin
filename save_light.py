a4956b6206929a402a9b95f7fc0fc2537bda0a85
shinjh0849 <shinjh0849@gmail.com>
2020-10-20 16:41:26 +0900

    fix tf seed error

--- save_light.py
+++ save_light.py
@@ -20,7 +20,7 @@ os.environ['PYTHONHASHSEED'] = str(seed_value)
 random.seed(seed_value)
 np.random.seed(seed_value)
 tf.random.set_seed(seed_value)
-session_conf = tf.ConfigProto(intra_op_parallelism_threads=1, inter_op_parallism_threads=1)
+session_conf = tf.compat.v1.ConfigProto(intra_op_parallelism_threads=1, inter_op_parallism_threads=1)
 sess = tf.Session(graph=tf.get_default_graph(), config=session_conf)
 K.set_session(sess)
 
